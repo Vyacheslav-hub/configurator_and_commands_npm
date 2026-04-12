1fafa34fa152221780f93179590e66a7d04f1242
Vyacheslav <slavik.kurganov.26@mail.ru>
2026-04-12 12:26:03 +0300

    Добавил файл webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+import path from "path";
+import { fileURLToPath } from "url";
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
+    entry: "./src/index.js",
+    devServer: {
+        static: './dist',
+        open: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html",
+        }),
+        new MiniCssExtractPlugin({
+            filename: "style.css",
+        }),
+    ],
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js",
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+            },
+        ],
+    },
+};
